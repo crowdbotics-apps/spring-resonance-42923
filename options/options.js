8addc90bbe32519af8abadc0f6c133c2c2a69c4e
Crowdbotics <team@crowdbotics.com>
2023-08-18 01:53:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "spring-resonance-42923",
+  url: "https://spring-resonance-42923.botics.co",
+  api: "https://spring-resonance-42923.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
